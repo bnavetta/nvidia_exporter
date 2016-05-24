68ee5ce3eb70e39577c98d3fbd7ec488f831de1e
Ben Navetta <ben.navetta@gmail.com>
2016-05-23 20:16:30 -0400

    base set of statistics

--- nvidia_exporter/main.py
+++ nvidia_exporter/main.py
@@ -1,11 +1,22 @@
 import pynvml
 
-from . import stats
+from .stats import NvidiaStats
 
 if __name__ == '__main__':
     try:
         pynvml.nvmlInit()
-        print(stats.gpu_count())
+        stats = NvidiaStats()
+        for device_index in range(0, stats.device_count()):
+            print("Device name", stats.device_name(device_index))
+            print("GPU temperature", stats.gpu_temp(device_index))
+            print("Fan speed", stats.device_fan_speed(device_index))
+            print("Power usage", stats.device_power_usage(device_index))
+            print("Power limit", stats.device_power_limit(device_index))
+            print("Free memory", stats.device_free_memory(device_index))
+            print("Used memory", stats.device_used_memory(device_index))
+            print("Total memory", stats.device_total_memory(device_index))
+            print("Graphics processes", stats.graphics_processes(device_index))
+            print("Compute processes", stats.compute_processes(device_index))
         pynvml.nvmlShutdown()
     except pynvml.NVMLError, err:
         print("NVML error: %s" % err)
--- nvidia_exporter/stats.py
+++ nvidia_exporter/stats.py
@@ -1,9 +1,47 @@
 import pynvml
 
-class NvidiaStats:
-    def gpu_count(self):
+class NvidiaStats(object):
+    def __init__(self):
+        self._devices = defaultdict(pynvml.nvmlDeviceGetHandleByIndex)
+
+    def device_count(self):
         return int(pynvml.nvmlDeviceGetCount())
 
-# running processes, GPU temp, memory usage, GPU speed?
+    def device_name(self, index):
+        return pynvml.nvmlDeviceGetName(self._devices[index])
 
-devicegetcomputrunningprocesses and devicegetgraphicsrunningprocesses
+    def gpu_temp(self, index):
+        return int(pynvml.nvmlDeviceGetTemperature(self._devices[index], pynvml.NVML_TEMPERATURE_GPU))
+
+    def device_fan_speed(self, index):
+        """Fan speed as a percentage"""
+        return int(pynvml.nvmlDeviceGetFanSpeed(self._devices[index]))
+
+    def device_power_usage(self, index):
+        """Power usage in milliwats"""
+        return int(pynvml.nvmlDeviceGetPowerUsage(self._devices[index]))
+
+    def device_power_limit(self, index):
+        """Power management limit in milliwats"""
+        return int(pynvml.nvmlDeviceGetPowerManagementLimit(self._devices[index]))
+
+    def device_free_memory(self, index):
+        return int(pynvml.nvmlDeviceGetMemoryInfo(self._devices[index]).free)
+
+    def device_total_memory(self, index):
+        return int(pynvml.nvmlDeviceGetMemoryInfo(self._devices[index]).total)
+
+    def device_used_memory(self, index):
+        return int(pynvml.nvmlDeviceGetMemoryInfo(self._devices[index]).used)
+
+    def compute_processes(self, index):
+        try:
+            return len(pynvml.nvmlDeviceGetComputeRunningProcesses(self._devices[index]))
+        except pynvml.NVMLError:
+            return -1
+
+    def graphics_processes(self, index):
+        try:
+            return len(pynvml.nvmlDeviceGetGraphicsRunningProcesses(self._devices[index]))
+        except pynvml.NVMLError:
+            return -1
