849236c91a25c00e6f9aadcffe03933c7f445e05
Ben Navetta <ben.navetta@gmail.com>
2016-05-23 20:45:00 -0400

    export metrics to prometheus

--- nvidia_exporter/main.py
+++ nvidia_exporter/main.py
@@ -1,10 +1,15 @@
+import atexit
+
 import pynvml
+import prometheus_client
 
 from .stats import NvidiaStats
+from .prometheus_metrics import build_metrics
 
 if __name__ == '__main__':
     try:
         pynvml.nvmlInit()
+        atexit.register(pynvml.nvmlShutdown)
         stats = NvidiaStats()
         for device_index in range(0, stats.device_count()):
             print("Device name", stats.device_name(device_index))
@@ -17,6 +22,9 @@ if __name__ == '__main__':
             print("Total memory", stats.device_total_memory(device_index))
             print("Graphics processes", stats.graphics_processes(device_index))
             print("Compute processes", stats.compute_processes(device_index))
-        pynvml.nvmlShutdown()
+
+        build_metrics()
+        prometheus_client.start_http_server(9200)
+
     except pynvml.NVMLError, err:
         print("NVML error: %s" % err)
