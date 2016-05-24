2548d24cf25832e4e1adf4a9590f5a7fb370b194
Ben Navetta <ben.navetta@gmail.com>
2016-05-23 21:48:35 -0400

    use wsgi to serve in foreground

--- nvidia_exporter/main.py
+++ nvidia_exporter/main.py
@@ -2,7 +2,8 @@ import atexit
 import time
 
 import pynvml
-import prometheus_client
+from prometheus_client import make_wsgi_app
+from wsgiref.simple_server import make_server
 
 from .stats import NvidiaStats
 from .prometheus_metrics import build_metrics
@@ -11,23 +12,10 @@ if __name__ == '__main__':
     try:
         pynvml.nvmlInit()
         atexit.register(pynvml.nvmlShutdown)
-        stats = NvidiaStats()
-        for device_index in range(0, stats.device_count()):
-            print("Device name", stats.device_name(device_index))
-            print("GPU temperature", stats.gpu_temp(device_index))
-            print("Fan speed", stats.device_fan_speed(device_index))
-            print("Power usage", stats.device_power_usage(device_index))
-            print("Power limit", stats.device_power_limit(device_index))
-            print("Free memory", stats.device_free_memory(device_index))
-            print("Used memory", stats.device_used_memory(device_index))
-            print("Total memory", stats.device_total_memory(device_index))
-            print("Graphics processes", stats.graphics_processes(device_index))
-            print("Compute processes", stats.compute_processes(device_index))
 
         build_metrics()
-        prometheus_client.start_http_server(9200)
-        while True:
-            time.sleep(10)	
-
+        app = make_wsgi_app()
+        httpd = make_server('', 9200, app)
+        httpd.serve_forever()
     except pynvml.NVMLError, err:
         print("NVML error: %s" % err)
